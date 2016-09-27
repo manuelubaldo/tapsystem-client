057f3a634017d5f4f0ad984539bf1b638ef7fa5e
manuelubaldo <klauel.ubaldo@gmail.com>
2016-09-27 18:40:26 +0800

    Add Bus Class

---
+++ app/src/main/java/com/tap/tapsystem/Bus.java
@@ -0,0 +1,16 @@
+package com.tap.tapsystem;
+
+/**
+ * Created by Lenovo ThinkPad X220 on 9/27/2016.
+ */
+public class Bus {
+    private String tPlateNo;
+
+    public String gettPlateNo() {
+        return tPlateNo;
+    }
+
+    public void settPlateNo(String tPlateNo) {
+        this.tPlateNo = tPlateNo;
+    }
+}
