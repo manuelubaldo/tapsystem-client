7db76b87729e4fb95ac3303c8f43656c5b5577e6
manuelubaldo <klauel.ubaldo@gmail.com>
2016-09-27 18:37:16 +0800

    Added Route, SeatNo, BusPlateNo property on Fair Charging Class

--- app/src/main/java/com/tap/tapsystem/FairCharging.java
+++ app/src/main/java/com/tap/tapsystem/FairCharging.java
@@ -4,11 +4,14 @@ package com.tap.tapsystem;
  * Created by Lenovo ThinkPad X220 on 9/18/2016.
  */
 public class FairCharging {
+    private String Route;
     private String Destination;
     private String CardNo;
     private String Remarks;
     private Double Balance;
     private String PhoneNo;
+    private String SeatNo;
+    private String BusPlateNo;
 
     public String getDestination() {
         return Destination;
@@ -48,5 +51,29 @@ public class FairCharging {
 
     public void setPhoneNo(String phoneNo) {
         PhoneNo = phoneNo;
+    }
+
+    public String getSeatNo() {
+        return SeatNo;
+    }
+
+    public void setSeatNo(String seatNo) {
+        SeatNo = seatNo;
+    }
+
+    public String getRoute() {
+        return Route;
+    }
+
+    public void setRoute(String route) {
+        Route = route;
+    }
+
+    public String getBusPlateNo() {
+        return BusPlateNo;
+    }
+
+    public void setBusPlateNo(String busPlateNo) {
+        BusPlateNo = busPlateNo;
     }
 }
