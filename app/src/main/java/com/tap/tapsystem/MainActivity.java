328b47dfeb0f191f582e5f307394c21d2c2e0cb7
manuelubaldo <klauel.ubaldo@gmail.com>
2016-09-27 18:38:32 +0800

    Added Seat and Bus Plate no

--- app/src/main/java/com/tap/tapsystem/MainActivity.java
+++ app/src/main/java/com/tap/tapsystem/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
-import android.view.accessibility.AccessibilityManager;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -44,27 +43,14 @@ public class MainActivity extends AppCompatActivity {
         RoutesSpinner = (Spinner) findViewById(R.id.routes);
         DestinationsSpinner = (Spinner) findViewById(R.id.destinations);
 
-        ArrayAdapter<CharSequence> routes = ArrayAdapter.createFromResource(this,R.array.routes,android.R.layout.simple_spinner_dropdown_item);
-        routes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        RoutesSpinner.setAdapter(routes);
-
-
-
-
         nfc = NfcAdapter.getDefaultAdapter(this);
         if (nfc==null){
             //Toast.makeText(MainActivity.this, "NFC Not Supported", Toast.LENGTH_LONG).show();
         }
 
         remarks = (TextView) findViewById(R.id.remarks);
-//        try{
-            loadSubRoutes();
-//        }catch (Exception ex){
-//            ArrayAdapter<CharSequence> destinations = ArrayAdapter.createFromResource(this,R.array.destinations,android.R.layout.simple_spinner_dropdown_item);
-//            destinations.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//            DestinationsSpinner.setAdapter(destinations);
-//            Toast.makeText(this,ex.getMessage(),Toast.LENGTH_LONG).show();
-//        }
+        loadSubRoutes();
+        loadBuses();
 
 
     }
@@ -73,13 +59,19 @@ public class MainActivity extends AppCompatActivity {
     public void OnClick(View view){
         EditText samplenfcno = (EditText) findViewById(R.id.samplenfcno);
         Spinner dest = (Spinner) findViewById(R.id.destinations);
+        Spinner route = (Spinner) findViewById(R.id.routes);
+        Spinner seats = (Spinner) findViewById(R.id.seats);
+        Spinner plateno = (Spinner) findViewById(R.id.plateno);
 
         remarks.setText("Please Wait...");
 //        api = ApiClient.client().create(ApiInterface.class);
         FairCharging fair = new FairCharging();
         fair.setCardNo(samplenfcno.getText().toString());
         fair.setDestination(dest.getSelectedItem().toString());
+        fair.setRoute(route.getSelectedItem().toString());
+        fair.setBusPlateNo(plateno.getSelectedItem().toString());
         fair.setRemarks("");
+        fair.setSeatNo(seats.getSelectedItem().toString());
         Call<FairCharging> call = api.charge(fair);
 
         call.enqueue(new Callback<FairCharging>() {
@@ -89,7 +81,13 @@ public class MainActivity extends AppCompatActivity {
 
                 remarks.setText(response.body().getRemarks());
                 SendSMS(response.body().getPhoneNo(),"Thank you for patronizing our Services.");
-
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        remarks.setText("Please Tap your Card");
+                    }
+                },1500);
             }
 
             @Override
@@ -143,6 +141,63 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG,t.toString());
             }
         });
+
+        Call<List<Routes>> call2 = api.getRoutes();
+        call2.enqueue(new Callback<List<Routes>>() {
+            @Override
+            public void onResponse(Call<List<Routes>> call, Response<List<Routes>> response) {
+                List<Routes> routes = response.body();
+                Log.d(TAG,"Length : " + routes.size());
+                List<String> route = new ArrayList<>();
+
+                for(Routes s : routes){
+                    route.add(s.getRouteName());
+                }
+                Log.d(TAG,"Length : " + route.size());
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this ,android.R.layout.simple_spinner_dropdown_item,route);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                RoutesSpinner.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Routes>> call, Throwable t) {
+                Log.d(TAG,t.toString());
+            }
+        });
+
+        List<String> seats = new ArrayList<>();
+        for(int i = 1 ;i<=60;i++){
+            seats.add("" + i);
+        }
+
+        ArrayAdapter<String> seatsAdapter = new ArrayAdapter<>(MainActivity.this,android.R.layout.simple_spinner_dropdown_item,seats);
+        seatsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner seatsSpinner = (Spinner) findViewById(R.id.seats);
+        seatsSpinner.setAdapter(seatsAdapter);
+    }
+
+    public void loadBuses(){
+        Call<List<Bus>> call = api.getBuses();
+        call.enqueue(new Callback<List<Bus>>() {
+            @Override
+            public void onResponse(Call<List<Bus>> call, Response<List<Bus>> response) {
+                List<Bus> buses = response.body();
+                List<String> bus = new ArrayList<>();
+                for(Bus b:buses){
+                    bus.add(b.gettPlateNo());
+                }
+                ArrayAdapter<String> busAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_spinner_dropdown_item,bus);
+                busAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                Spinner BusSpinner = (Spinner) findViewById(R.id.plateno);
+                BusSpinner.setAdapter(busAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Bus>> call, Throwable t) {
+
+            }
+        });
+
     }
 
 }
