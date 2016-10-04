b2bb0a97299de507d417ecd104173a65823560d5
manuelubaldo <klauel.ubaldo@gmail.com>
2016-10-05 01:47:28 +0800

    Bus Assignment Class

---
+++ app/src/main/java/com/tap/tapsystem/BusAssignment.java
@@ -0,0 +1,18 @@
+package com.tap.tapsystem;
+
+/**
+ * Created by Lenovo ThinkPad X220 on 10/4/2016.
+ */
+
+public class BusAssignment {
+    public int iConductorID;
+    public String tConductionName;
+    public int iDriveID;
+    public String tDriverName;
+    public int iInspectorID;
+    public String tInspectorName;
+    public int iBusID;
+    public String tBusPlateNo;
+    public int iRouteID;
+    public String tRouteName;
+}
